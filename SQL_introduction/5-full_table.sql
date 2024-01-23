8d99bca9b050243e1a2857f07bd00ea777e50587
Nicholas Simwichi <nsimwichi@gmail.com>
2024-01-23 20:30:07 +0300

    Full table

---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,7 @@
+-- A script to create a table called first_table
+
+-- USE DATABASE;                -- If you want to pass the database name on command line you dont include this line
+
+USE hbtn_0c_0;
+
+SHOW COLUMNS FROM first_table;
