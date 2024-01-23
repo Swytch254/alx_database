e2017cf7ec9592d4e18b381ddb02763fe0c56b9a
Nicholas Simwichi <nsimwichi@gmail.com>
2024-01-23 17:39:32 +0300

    some changes

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -7,7 +7,7 @@ CREATE SCHEMA IF NOT EXISTS hbtn_0c_0;
 USE hbtn_0c_0;
 
 CREATE TABLE IF NOT EXISTS first_table
-(
-    id INT,
-    name VARCHAR(256)
-);
+    (
+        id INT,
+        name VARCHAR(256)
+    );
