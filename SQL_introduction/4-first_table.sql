70e3955fb91f7e32ea92b3f408ca57ad672ace61
Nicholas Simwichi <nsimwichi@gmail.com>
2024-01-23 17:34:35 +0300

    creating tables

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,13 @@
+-- A script to create a table called first_table
+
+-- USE DATABASE;                -- If you want to pass the database name on command line you dont include this line
+
+CREATE SCHEMA IF NOT EXISTS hbtn_0c_0;
+
+USE hbtn_0c_0;
+
+CREATE TABLE IF NOT EXISTS first_table
+(
+    id INT,
+    name VARCHAR(256)
+);
