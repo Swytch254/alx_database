cb9b3105a7a20180304db25ced52951e06b85dae
Nicholas Simwichi <nsimwichi@gmail.com>
2024-01-23 15:55:09 +0300

    list tables

---
+++ SQL_introduction/3-list_tables.sql
@@ -0,0 +1,5 @@
+-- A script to list a database that will be passed as an argument in the command prompt
+
+-- USE DATABASE;                --If you want to pass the database name on command line you dont include this line
+
+SHOW TABLES;
