1c1b2f5335252cedc046d9395fac5ad8df9dc995
Nicholas Simwichi <nsimwichi@gmail.com>
2024-01-24 12:02:08 +0300

    database edit

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,10 +1,10 @@
 -- A script to create a table called first_table
 
--- USE DATABASE;                -- If you want to pass the database name on command line you dont include this line
+-- USE DATABASE If you want to pass the database name on command line you dont include this line
 
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
 
-USE hbtn_0c_0;
+-- USE hbtn_0c_0;
 
 CREATE TABLE IF NOT EXISTS first_table
     (
