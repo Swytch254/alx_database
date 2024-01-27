adfa8e320fb098eb146ff51b7f6fc8e92599fe43
Nicholas Simwichi <nsimwichi@gmail.com>
2024-01-27 13:10:31 +0300

    changes

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -2,10 +2,6 @@
 
 -- USE DATABASE If you want to pass the database name on command line you dont include this line
 
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
-
--- USE hbtn_0c_0;
-
 CREATE TABLE IF NOT EXISTS first_table
     (
         id INT,
--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
--- A script to create a table called first_table
+-- A script to show description of a table called first_table without using DESCRIBE or EXPLAIN
 
 SHOW CREATE TABLE `first_table`;
