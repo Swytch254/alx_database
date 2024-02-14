8814108e0af64a4d47046d9694a958bde314a35a
Nicholas Simwichi <nsimwichi@gmail.com>
2024-02-14 22:36:11 +0300

    privs again again

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -3,10 +3,8 @@
 -- set user_0d_2 password to user_0d_2_pwd
 -- if user and database exist script doesn't fail
 
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
-CREATE USER IF NOT EXISTS `user_0d_2`@`localhost`;
-
-SET PASSWORD FOR `user_0d_2`@`localhost`=`user_0d_2_pwd`; 
-
-GRANT SELECT ON hbtn_0d_2.* TO `user_0d_2`@`localhost`; 
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
