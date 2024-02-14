dd27d4067c2e95994d025c8a47e105bb23f78948
Nicholas Simwichi <nsimwichi@gmail.com>
2024-02-14 08:20:02 +0300

    database and user

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,12 @@
+-- create a database hbtn_0d_2 and user_0d_2
+-- user_0d_2 to have only SELECT privilege
+-- set user_0d_2 password to user_0d_2_pwd
+-- if user and database exist script should not fail
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+
+SET PASSWORD FOR 'user_0d_2'@'localhost'='user_0d_2_pwd'; 
+
+GRANT SELECT ON hbtn_0d_2 TO 'user_0d_2'@'localhost'; 
